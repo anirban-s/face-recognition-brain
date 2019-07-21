06a0701c96e7615725e084dd04f68af9b58cdf4c
anirban-s <baban.chrome@gmail.com>
2019-07-22 02:33:48 +0530

    face recognition api is implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import './App.css';
 import Navigation from './component/Navigation/Navigation';
 import Logo from './component/Logo/Logo';
 import Rank from './component/Rank/Rank';
 import ImageLinkForm from './component/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './component/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
 
 const particlesOptions = {
@@ -18,17 +20,53 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles' params={ particlesOptions }/>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-            {/*<FaceRecognition /> */}
-    </div>
-  );
+const app = new Clarifai.App({
+ apiKey: '496072ef859148188eb41c5faefdd9e8'
+});
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+
+    app.models.predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
+  render(){
+      return (
+        <div className="App">
+          <Particles className='particles' params={ particlesOptions }/>
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit } />
+          <FaceRecognition imageUrl={this.state.imageUrl}/>
+        </div>
+      );
+    }
 }
 
 export default App;
--- src/component/ImageLinkForm/ImageLinkForm.js
+++ src/component/ImageLinkForm/ImageLinkForm.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
         {'This magic brain will detect faces in your picture'}
       </p>
       <div className="center form shadow-3">
-        <input type='text' className='f4 pa2 w-70 center'/>
-        <button className='w-30 grow f4 link ph3 pv dib white bg-light-purple'>Detect</button>
+        <input type='text' className='f4 pa2 w-70 center' onChange={ onInputChange }/>
+        <button className='w-30 grow f4 link ph3 pv dib white bg-light-purple' onClick={ onButtonSubmit }>Detect</button>
       </div>
     </div>
   );
